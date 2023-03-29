62230b633ee18f333440115d7c6800a142afefa1
+ <+>
2023-03-29 00:21:20 -0400

    commit tree 49f40ffcaf6d50325e0069322bec5a1d1c9e38ba

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,18 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json:{ errors: invalid.record.errors }, status: :unprocessable_entity
   end
+  # def update
+  #   bird = find_bird
+  #   bird.update!(bird_params)
+  #   render json: bird
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
 
   private
 
